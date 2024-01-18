bd1b4dc5a2198367d4f033521a11239dbd6e6867
S-Jhinkoe <302907218@student.rocmondriaan.nl>
2024-01-18 09:58:31 +0100

    bewegende snake

--- main.js
+++ main.js
@@ -9,9 +9,13 @@ let context;
 let snakeX = blocksize * 5;
 let snakeY = blocksize * 5;
 
+let velocityX = 0;
+let velocityY = 0;
+
+
 //powerup///
-let powerupX = blocksize * 10;
-let powerupY = blocksize * 10;
+let powerupX;
+let powerupY;
 
 window.onload = function() {
     board = document.querySelector("#board");
@@ -19,7 +23,34 @@ window.onload = function() {
     board.width = cols * blocksize;
     context = board.getContext("2d");
 
-    update();
+    placePowerup()
+   
+    document.addEventListener("keyup", changeDirection);
+
+     //update();
+     setInterval(update,1000/10); ////100 miliseconden
+}
+
+function changeDirection(i) {
+if (i.code == "ArrowUp") {
+    velocityX = 0;
+    velocityY = -1;
+}
+
+else if (i.code == "ArrowDown") {
+    velocityX = 0;
+    velocityY = 1;
+}
+
+else if (i.code == "ArrowLeft") {
+    velocityX = -1;
+    velocityY = 0;
+}
+
+else if (i.code == "ArrowRight") {
+    velocityX = 1;
+    velocityY = 0;
+}
 }
 
 function update() {
@@ -27,8 +58,16 @@ function update() {
     context.fillRect(0,0, board.width, board.height);
 
     context.fillStyle = "lime";
+    snakeX += velocityX;
+    snakeY += velocityY;
     context.fillRect(snakeX, snakeY, blocksize, blocksize);
 
     context.fillStyle = "red";
     context.fillRect(powerupX, powerupY, blocksize, blocksize);
+
+}
+
+function placePowerup() {
+    powerupX =  Math.floor(Math.random() * cols) * blocksize;
+    powerupY =  Math.floor(Math.random() * rows) * blocksize;
 }
